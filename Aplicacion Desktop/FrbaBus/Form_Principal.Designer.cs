6ca44569506e93295f5c2fa75e859d2834305048
morales-franco <franco.e.morales@gmail.com>
2013-07-21 22:39:41 -0300

    update form_principal

--- Aplicacion Desktop/FrbaBus/Form_Principal.Designer.cs
+++ Aplicacion Desktop/FrbaBus/Form_Principal.Designer.cs
@@ -39,9 +39,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(66, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(369, 37);
+            this.label1.Size = new System.Drawing.Size(351, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Bienvenido a FRBABus";
+            this.label1.Text = "Bienvenido a FrbaBus";
             // 
             // label2
             // 
